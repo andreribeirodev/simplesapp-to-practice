fa645df5e5f6ba0a37763bac39b16e50798cfb60
andreribeirodev <andreeleeandro2@gmail.com>
2022-05-14 01:02:07 -0300

    first app to practice fragments/finished

--- simpleapptopractice-fragments/app/src/main/java/com/andreribeiro/simpleapptopractice_fragments/MainActivity.kt
+++ simpleapptopractice-fragments/app/src/main/java/com/andreribeiro/simpleapptopractice_fragments/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.andreribeiro.simpleapptopractice_fragments
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.andreribeiro.simpleapptopractice_fragments.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val homeFragment by lazy { HomeFragment() }
+    private val consoleFragment by lazy { ConsoleFragment() }
+    private val gameFragment by lazy { GamesFragment() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.btnHome.setOnClickListener(this)
+        binding.btnConsole.setOnClickListener(this)
+        binding.btnGames.setOnClickListener(this)
+    }
+
+    override fun onClick(btn: View) {
+        when (btn) {
+            binding.btnHome -> {
+                btn.setOnClickListener { setFragment(homeFragment) }
+            }
+            binding.btnConsole -> {
+                btn.setOnClickListener { setFragment(consoleFragment) }
+            }
+            binding.btnGames -> {
+                btn.setOnClickListener { setFragment(gameFragment) }
+            }
+        }
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(binding.fragmentContainerMain.id, fragment)
+        fragmentTransaction.commit()
     }
 }
